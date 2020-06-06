6df8e3177a4857bbd7a9debb9633ba26f52dc4c3
Sandeep Devalla <sandeepdevalla@Sandeeps-MacBook-Pro.local>
2020-06-06 23:01:53 +0530

    Updates

---
+++ precache-manifest.1e2a51d800d694e376a232b400f97242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e9285e713a6d8bdf2725c81787e698",
+    "url": "/bslv-constructions-nellore/index.html"
+  },
+  {
+    "revision": "d2912d02609b505f3c31",
+    "url": "/bslv-constructions-nellore/static/css/main.800cba81.chunk.css"
+  },
+  {
+    "revision": "a295adef6aea2ad9494f",
+    "url": "/bslv-constructions-nellore/static/js/2.9b037310.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bslv-constructions-nellore/static/js/2.9b037310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2912d02609b505f3c31",
+    "url": "/bslv-constructions-nellore/static/js/main.e41f4592.chunk.js"
+  },
+  {
+    "revision": "f66f72f72c42b8c03059",
+    "url": "/bslv-constructions-nellore/static/js/runtime-main.f1689a81.js"
+  }
+]);
