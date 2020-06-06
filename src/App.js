507985a012e46b1ae01e16730a97cdfaeb9ea7ee
Sandeep Devalla <sandeepdevalla@Sandeeps-MacBook-Pro.local>
2020-06-06 22:55:42 +0530

    initial commit with introduction to bslv

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <address>Address- 27-4-238, krishnaveni nilayam, ground floor, 3rd street , Ramji nagar, opp saibaba temple Street, children's park road, nellore - 524002</address>
+        <img src="./bslvlogo.jpeg" className="App-logo" alt="logo" />
+        <div>
+          <b>Contact : </b> +91 7095029123
+        </div>
+        <div> 
+          <b>Website : </b>bslvconstructions@gmail.com
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
